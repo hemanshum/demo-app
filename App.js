9df56e0140dcacd2c7d7a3209b22c2279c55de6f
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2023-04-10 21:58:16 +0530

    Custom Font added

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    "mrt-mid": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "mrt-bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "mrt-xbold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={handleOnLayout}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.text}>Enjoy the beauty of your custom fonts!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "mrt-xbold",
+    fontSize: 30,
+  },
+  text: {
+    fontFamily: "mrt-mid",
+    fontSize: 16,
   },
 });
